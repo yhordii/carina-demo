a77422c06ca1425a5f974dea8ea856f82c4b06ca
vdelendik <vadim.delendik@gmail.com>
2021-03-02 20:06:09 +0300

    assigned x-ray test label

--- src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/APISampleTest.java
@@ -31,6 +31,7 @@ import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
 import com.qaprosoft.carina.demo.api.DeleteUserMethod;
 import com.qaprosoft.carina.demo.api.GetUserMethods;
 import com.qaprosoft.carina.demo.api.PostUserMethod;
+import com.zebrunner.agent.core.annotation.TestLabel;
 
 /**
  * This sample shows how create REST API tests.
@@ -41,6 +42,7 @@ public class APISampleTest extends AbstractTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     
 
+    @TestLabel(name = "com.zebrunner.app/tcm.xray.test-key", value = {"XTEST1-4"})
     @Test(description = "JIRA#DEMO-0001")
     @MethodOwner(owner = "qpsdemo")
     public void testCreateUser() throws Exception {
@@ -50,37 +52,6 @@ public class APISampleTest extends AbstractTest {
         api.expectResponseStatus(HttpResponseStatusType.CREATED_201);
         api.callAPI();
         api.validateResponse();
-    }
-
-    @Test(description = "JIRA#DEMO-0002")
-    @MethodOwner(owner = "qpsdemo")
-    public void testCreateUserMissingSomeFields() throws Exception {
-        PostUserMethod api = new PostUserMethod();
-        api.getProperties().remove("name");
-        api.getProperties().remove("username");
-        api.expectResponseStatus(HttpResponseStatusType.CREATED_201);
-        api.callAPI();
-        api.validateResponse();
-    }
-
-    @Test(description = "JIRA#DEMO-0003")
-    @MethodOwner(owner = "qpsdemo")
-    public void testGetUsers() {
-        GetUserMethods getUsersMethods = new GetUserMethods();
-        getUsersMethods.expectResponseStatus(HttpResponseStatusType.OK_200);
-        getUsersMethods.callAPI();
-        getUsersMethods.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
-        getUsersMethods.validateResponseAgainstSchema("api/users/_get/rs.schema");
-    }
-
-    @Test(description = "JIRA#DEMO-0004")
-    @MethodOwner(owner = "qpsdemo")
-    @TestPriority(Priority.P1)
-    public void testDeleteUsers() {
-        DeleteUserMethod deleteUserMethod = new DeleteUserMethod();
-        deleteUserMethod.expectResponseStatus(HttpResponseStatusType.OK_200);
-        deleteUserMethod.callAPI();
-        deleteUserMethod.validateResponse();
     }
 
 }
